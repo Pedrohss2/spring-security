ae6bb678318f482210c80be9568c09559a05efa4
Pedro Henrique <pedro2266809ph@gmail.com>
2024-02-29 17:07:45 -0300

    BCrypt encoder password

--- src/main/java/com/pedro/demo/conf/SecurityConfig.java
+++ src/main/java/com/pedro/demo/conf/SecurityConfig.java
@@ -3,10 +3,18 @@ package com.pedro.demo.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpj) throws Exception {
@@ -15,5 +23,7 @@ public class SecurityConfig {
         httpj.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return httpj.build();
     }
+
+
 }
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$iSeNGcBfAycbPqyW2L059eqIHD7oN3H8cB2l5Z.bSRkXsxfY1EDc6');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$iSeNGcBfAycbPqyW2L059eqIHD7oN3H8cB2l5Z.bSRkXsxfY1EDc6');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
