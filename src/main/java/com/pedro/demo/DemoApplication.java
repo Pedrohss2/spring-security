fcc554e7b6c8c677e8693fbfcb7798db7a5d5e0a
Pedro Henrique <pedro2266809ph@gmail.com>
2024-03-01 08:43:01 -0300

    refactor: chaging

--- src/main/java/com/pedro/demo/DemoApplication.java
+++ src/main/java/com/pedro/demo/DemoApplication.java
@@ -1,11 +1,7 @@
 package com.pedro.demo;
 
-import com.pedro.demo.conf.SecurityConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication   {
