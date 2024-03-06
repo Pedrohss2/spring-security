94be34951387ab0c05e5fa21ddd633ac14473f3f
Pedro Henrique <pedro2266809ph@gmail.com>
2024-03-06 16:57:45 -0300

    creating a class TokenService

---
+++ src/main/java/com/pedro/demo/services/TokenService.java
@@ -0,0 +1,39 @@
+package com.pedro.demo.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.pedro.demo.entities.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("{api.security.token.secret}")
+    private String secret;
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create().withIssuer("api")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(generationExpirationToken())
+                    .sign(algorithm);
+            return token;
+        }
+        catch (JWTCreationException error) {
+            throw new RuntimeException("Error while generate toke: " + error);
+        }
+    }
+
+    private Instant generationExpirationToken() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
